4fd6371960ead1ec67032a376db622d268b574d1
Isaac Caires <zrfisaac@gmail.com>
2025-02-09 10:37:13 +0000

    zrfisaac.docker.compose.noip : 0.0.1

---
+++ noip/purge.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+$sudo docker container stop noip
+$sudo docker container rm noip
