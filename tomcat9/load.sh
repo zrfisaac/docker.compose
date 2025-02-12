e2addd6e34fec8c750a5ba24eebaad5a61dd7201
Isaac Caires <zrfisaac@gmail.com>
2025-02-12 19:04:37 +0000

    zrfisaac.docker.compose.tomcat9 : 0.0.1

---
+++ tomcat9/load.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+mkdir -p ./public
+mkdir -p ./webapps
+$sudo docker compose -f docker-compose.yml up -d
---
+++ tomcat9/purge.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+$sudo docker container stop tomcat9
+$sudo docker container rm tomcat9
