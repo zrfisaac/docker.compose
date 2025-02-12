f20ba0b7ea57c6d8c287cda01a589bf5022825a8
Isaac Caires <zrfisaac@gmail.com>
2025-02-12 19:04:30 +0000

    zrfisaac.docker.compose.postgres14 : 0.0.1

---
+++ postgres14/purge.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+$sudo docker container stop postgres14
+$sudo docker container rm postgres14
